6c82ba09c66a059c148fa93e84643c5aa66a585a
Victor Lombardi <Victor.Lombardi@lbv.org.br>
2023-03-22 10:39:55 -0300

    Criação da classe notícia

---
+++ js/Noticia.js
@@ -0,0 +1,14 @@
+export class Noticia {
+    titulo
+    subtitulo
+    texto
+    imagem
+    cor
+    constructor(titulo, subtitulo, imagem) {
+        this.titulo = titulo
+        this.subtitulo = subtitulo
+        this.texto = texto
+        this.imagem = imagem
+        this.cor = cor
+    }
+}
--- js/criaNoticia.js
+++ js/criaNoticia.js
@@ -1,16 +1,20 @@
+import { Noticia } from "./Noticia.js";
+
 let sectionNoticias = document.getElementById('noticias')
 let adicionaNoticia = JSON.parse(localStorage.getItem("adicionaNoticia"))
 let imgStorage = JSON.parse(localStorage.getItem('img')) || [];
 let titulo = JSON.parse(localStorage.getItem('titulo')) || [];
 let subtitulo = JSON.parse(localStorage.getItem('subtitulo')) || [];
 
+let noticiaNova = new Noticia(titulo, subtitulo, imgStorage)
+
 if(adicionaNoticia == 1) {
     sectionNoticias.innerHTML += `
         <div class="card noticia d-inline-block col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-1 col-1" >
-            <img src="${imgStorage[0].url}" class="imagem-fome" alt="${imgStorage[0].alt}">
+            <img src="${noticiaNova.imgStorage[0].url}" class="imagem-fome" alt="${noticiaNova.imgStorage[0].alt}">
             <div class="card-body">
-                <h5 class="card-title titulo-importante">${titulo[0].texto}</h5>
-                <p class="card-text" id="p-do-ngc-la">${subtitulo[0].texto}</p>
+                <h5 class="card-title titulo-importante">${noticiaNova.titulo[0].texto}</h5>
+                <p class="card-text" id="p-do-ngc-la">${noticiaNova.subtitulo[0].texto}</p>
                 <a href="novaNoticia.html" class="btn btn-primary btn-card">Saiba mais!</a>
             </div>
         </div>
